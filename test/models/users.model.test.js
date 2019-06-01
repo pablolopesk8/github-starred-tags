4ce9527cb620e7feacdb35c1387d288d50a2426a
Pablo Lopes <>
2019-05-31 21:59:51 -0300

    Created commands to watch files for test Created the test for db service

---
+++ test/models/users.model.test.js
@@ -0,0 +1,26 @@
+const should = require('should'); // eslint-disable-line
+
+describe('Model User Test', () => {
+    describe('Model validation tests', () => {
+        it('Should be have a validation for githubUser required', () => {
+        });
+
+        it('Should be have a validation for starred repositories, if exists, the minimun one', () => {
+        });
+
+        it('Should be have a validation for starred repositories, if exists, required github, name and url', () => {
+        });
+
+        it('Should be created only with githubUser', () => {
+        });
+
+        it('Should be created with githubUser and some other properties', () => {
+        });
+
+        it('Should be created with repositories, but without starred property', () => {
+        });
+
+        it('Should be created with repositories, and with starred property', () => {
+        });
+    });
+});
